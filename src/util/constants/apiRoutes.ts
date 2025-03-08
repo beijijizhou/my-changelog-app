3b7ad29eefa68691b0a6e273e69f00498eb393e0
“beijijizhou” <hongzhong.hu@stonybrook.edu>
2025-03-08 05:29:11 -0500

    reconfigure backend

--- src/util/constants/apiRoutes.ts
+++ src/util/constants/apiRoutes.ts
@@ -1,3 +1,5 @@
-export const BACKEND_ROUTE = "http://localhost:5000"
+const REMOTE_BACKEND_ROUTE = "https://my-changelog-backend.vercel.app/";
+const BACKEND_ROUTE = import.meta.env.VITE_API_BACKEND_ROUTE || REMOTE_BACKEND_ROUTE;
+console.log(BACKEND_ROUTE)
 export const GITHUB_CALLBACK = "/callback"
 export const GITHUB_CALLBACK_API_ROUTE = BACKEND_ROUTE + GITHUB_CALLBACK;

b59a007d50cd60e988d9f8c2f42902033463ad58
“beijijizhou” <hongzhong.hu@stonybrook.edu>
2025-03-10 15:28:56 -0400

    testing production bug

--- src/pages/Dashboard/components/Editor.tsx
+++ src/pages/Dashboard/components/Editor.tsx
@@ -11,6 +11,7 @@ const Editor = ({ initialContent }: { initialContent: string }) => {
   useEffect(() => {
     setEditorValue(initialContent);
   }, [initialContent]);
+  console.log(initialContent)
   return (
     <div>
       <h1>Commit Summary</h1>

eca127f78fdbb0dcef5df0e6a2fe06a78df73607
“beijijizhou” <hongzhong.hu@stonybrook.edu>
2025-03-14 14:23:46 -0400

    troubleshoot: display token in outside

--- src/util/interceptor.ts
+++ src/util/interceptor.ts
@@ -3,11 +3,11 @@ import axios from "axios";
 import { LocalStorageNames } from "./constants/localstorage";
 export const configureAxiosInterceptors = () => {
   const token = localStorage.getItem(LocalStorageNames.TOKEN);
+  console.log(token)
   axios.interceptors.request.use(
     async (config: any) => {
       if (token) {
         config.headers["Authorization"] = `Bearer ${token}`;
-        console.log(token)
       }
       config.withCredentials = true;
       return config;

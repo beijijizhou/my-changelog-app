f697f16256b7f24166a889adc2b2648b5806c06a
“beijijizhou” <hongzhong.hu@stonybrook.edu>
2025-03-14 14:43:40 -0400

    fix: token variable is not configured properly

--- src/util/interceptor.ts
+++ src/util/interceptor.ts
@@ -2,10 +2,11 @@
 import axios from "axios";
 import { LocalStorageNames } from "./constants/localstorage";
 export const configureAxiosInterceptors = () => {
-  const token = localStorage.getItem(LocalStorageNames.TOKEN);
-  console.log(token)
+
   axios.interceptors.request.use(
+   
     async (config: any) => {
+      const token = localStorage.getItem(LocalStorageNames.TOKEN);
       if (token) {
         config.headers["Authorization"] = `Bearer ${token}`;
       }
